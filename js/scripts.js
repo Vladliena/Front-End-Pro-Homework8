409802caecc9656b3955847cf86f3666ef85a354
vladliena <sotnikovavladlena@gmail.com>
2022-12-15 18:51:39 +0200

    Deleted space

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-const studentsArrayOne = ['John','Sam','Scott','Lisa','Bob','Paul','Lola','Andrew','Vladimir','Inna'];
+const studentsArrayOne = ['John', 'Sam', 'Scott', 'Lisa', 'Bob', 'Paul', 'Lola', 'Andrew', 'Vladimir', 'Inna'];
 
-studentsArrayOne.push('Mathew','Tom');
+studentsArrayOne.push('Mathew', 'Tom');
 
-studentsArrayOne.unshift('Agata','Nick');
+studentsArrayOne.unshift('Agata', 'Nick');
 
 studentsArrayOne.pop();
 
 studentsArrayOne.shift();
 
 const studentCheck = prompt('Tell me the name');
-    if (studentsArrayOne.includes(studentCheck)){
-        console.log(`${studentCheck} is here`);
-    } else {
-        console.log(`${studentCheck} is not here`);
-    }
+if (studentsArrayOne.includes(studentCheck)) {
+    console.log(`${studentCheck} is here`);
+} else {
+    console.log(`${studentCheck} is not here`);
+}
 
-const studentsArrayTwo = studentsArrayOne.slice(3,7);
+const studentsArrayTwo = studentsArrayOne.slice(3, 7);
 
-    const studentsArrayThree = ['Rebeca','Mark','George','Antony','Ben','Claud'];
-    const allStudents = studentsArrayTwo.concat(studentsArrayThree);
+const studentsArrayThree = ['Rebeca', 'Mark', 'George', 'Antony', 'Ben', 'Claud'];
+const allStudents = studentsArrayTwo.concat(studentsArrayThree);
 
-allStudents.splice(4,1);
-allStudents.splice(6,1,'Ivan')
+allStudents.splice(4, 1);
+allStudents.splice(6, 1, 'Ivan')
 
 console.log(allStudents.join('-'))
