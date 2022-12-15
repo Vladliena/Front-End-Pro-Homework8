6cedc263e588387599fc57b513e64b78a528564e
vladliena <sotnikovavladlena@gmail.com>
2022-12-15 12:23:29 +0200

    Last commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
 const studentsArrayOne = ['John', 'Sam', 'Scott', 'Lisa','Bob','Paul', 'Lola', 'Andrew', 'Vladimir', 'Inna'];
 
 studentsArrayOne.push('Mathew','Tom');
-// console.log(studentsArrayOne);
 
 studentsArrayOne.unshift('Agata','Nick');
-// console.log(studentsArrayOne);
 
 studentsArrayOne.pop();
-// console.log(studentsArrayOne);
 
 studentsArrayOne.shift();
-// console.log(studentsArrayOne);
 
 const studentCheck = prompt('Tell me the name');
     if (studentsArrayOne.includes(studentCheck)){
@@ -18,7 +14,6 @@ const studentCheck = prompt('Tell me the name');
     } else console.log(`${studentCheck} is not here`);
 
 const studentsArrayTwo = studentsArrayOne.slice(3,7);
-    // console.log(studentsArrayTwo);
 
     const studentsArrayThree = ['Rebeca', 'Mark', 'George','Antony','Ben','Claud'];
     const allStudents = studentsArrayTwo.concat(studentsArrayThree);
